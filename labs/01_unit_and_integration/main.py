3e10e0e4d7768c7a2d3fd67b8f4a962c2625858d
Konstantina Konstantinidou <kkonstantina@hotmail.com>
2025-08-22 14:41:13 +0200

    test fork change

--- labs/01_unit_and_integration/main.py
+++ labs/01_unit_and_integration/main.py
@@ -2,30 +2,36 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
+"I love python"
+
+
 def format_response(data):
-    return {"result": data }
+    return {"result": data}
 
 
 @app.get("/addition")
 def addition(a: int, b: int):
-    response_int = (a + b)
+    response_int = a + b
     response_object = format_response(response_int)
     return response_object
+
 
 @app.get("/substraction")
 def substraction(a: int, b: int):
-    response_int = (a - b)
+    response_int = a - b
     response_object = format_response(response_int)
     return response_object
+
 
 @app.get("/multiplication")
 def multiplication(a: int, b: int):
-    response_int = (a * b)
+    response_int = a * b
     response_object = format_response(response_int)
     return response_object
 
+
 @app.get("/division")
 def division(a: int, b: int):
-    response_int = (a / b)
+    response_int = a / b
     response_object = format_response(response_int)
     return response_object
